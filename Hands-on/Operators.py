5986d01a6c855d722d9f8dd47527cc0a1f239fa2
Venkata Sri Deepthi SriKotaPeetambaram <165433978+SKPVenkataSDeepthi@users.noreply.github.com>
2025-01-02 16:28:12 -0500

    operators

---
+++ Hands-on/Operators.py
@@ -0,0 +1,11 @@
+# Statements and Expressions 
+username = "Aviator56"
+print(username == "Aviator56")
+
+text = input("Enter some text:") ### input() is actual data entered by the user 
+print(text)
+
+pi =  3.14159265359
+print(f"The value of pi is approximately:{pi:.3f}.") ### formatted output ... if f is not there in front of the quotations then it will only give the ones present in the quotations
+
+
